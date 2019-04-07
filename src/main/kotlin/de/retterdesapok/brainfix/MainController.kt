c5a8142bb21996027c2b07adb6d6fbe518f4e860
Andreas <andreas.fichtner@gmail.com>
2019-04-07 15:13:14 +0200

    Don' track properties file

--- src/main/kotlin/de/retterdesapok/brainfix/MainController.kt
+++ src/main/kotlin/de/retterdesapok/brainfix/MainController.kt
@@ -18,9 +18,8 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import java.util.*
 import javax.servlet.http.HttpServletResponse
-
+import java.util.*
 
 @Controller
 class MainController {
